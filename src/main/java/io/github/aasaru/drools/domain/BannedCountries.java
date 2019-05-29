6f56823c76ce75950d5d7a60bc12000d180ba5d7
Zoltan Lajko <zoltan.lajko@ericsson.com>
2019-05-29 15:07:01 +0200

    missing file BannedCountries added to the commit

---
+++ src/main/java/io/github/aasaru/drools/domain/BannedCountries.java
@@ -0,0 +1,9 @@
+package io.github.aasaru.drools.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BannedCountries {
+    public static final List<String> bannedCountries = Arrays.asList("Germany", "Romania");
+
+}
